7213211657f77fdaf4a9f4b36036fa7d214f34a3
Sophie Hao <yiding.hao@yale.edu>
2023-02-03 07:45:55 -0500

    Added support for caching data

--- load_data.py
+++ load_data.py
@@ -1,12 +1,19 @@
 import csv
 import os
 from pathlib import Path
-from typing import Dict
 
 from datasets import Dataset, load_dataset, concatenate_datasets
 
+_all_data = None
+
 
 def _add_metadata_file(folder: Path):
+    """
+    Adds a metadata.csv file to a folder that lists the shape and
+    texture features of each image in the folder.
+
+    :param folder: The folder to add the metadata.csv file to
+    """
     with open(folder / "metadata.csv", "w") as o:
         writer = csv.writer(o)
         writer.writerow(["file_name", "shape", "texture"])
@@ -35,6 +42,16 @@ def _load_folder(folder: str, root: Path = Path("data/")) -> Dataset:
     return load_dataset("imagefolder", data_dir=folder)["train"]
 
 
-def load_all_data(root: Path = Path("data/")) -> Dict[str, Dataset]:
-    return concatenate_datasets([_load_folder(f, root=root)
-                                 for f in os.listdir(root) if f.isalnum()])
+def load_all_data(root: Path = Path("data/")) -> Dataset:
+    """
+    Loads all images from the Gheiros dataset.
+
+    :param root: The root directory of the Gheiros dataset
+    :return: The loaded data
+    """
+    global _all_data
+    if _all_data is None:
+        datasets = [_load_folder(f, root=root)
+                    for f in os.listdir(root) if f.isalnum()]
+        _all_data = concatenate_datasets(datasets)
+    return _all_data

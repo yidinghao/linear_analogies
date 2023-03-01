fe447cae7d6e64004c2053092f8390d1f19be41c
Sophie Hao <yiding.hao@yale.edu>
2023-03-01 11:28:11 -0500

    Updated get_vectors with all models

--- get_vectors.py
+++ get_vectors.py
@@ -1,27 +1,34 @@
+"""
+Script to extract image representations from models.
+"""
+import json
 import pickle
+from pathlib import Path
 
 import torch
 import torch.nn as nn
 from tqdm import tqdm
 
 from load_data import load_all_data
-from models import ViTModel, AlexNetModel
-from preprocessors import Preprocessor, ViTPreprocessor, AlexNetPreprocessor
+from models import *
+from preprocessors import *
 
 
 def get_vectors(model: nn.Module, preprocessor: Preprocessor,
-                batch_size: int = 5, output_filename: str = "vectors.p",
+                batch_size: int = 5, output_filename: Path = Path("vectors.p"),
                 use_cuda: bool = True):
     """
-    Extract representations
+    Extracts image representations from a model and saves them to a
+    pickle file.
 
-    :param model:
-    :param preprocessor:
-    :param batch_size:
-    :param use_cuda:
-    :return:
+    :param model: The model to extract image representations from
+    :param preprocessor: The preprocessor for the model
+    :param batch_size: The batch size to use for the model
+    :param output_filename: The file to save the representations to
+    :param use_cuda: Whether or not to use GPU
     """
     data = load_all_data(preprocessor=preprocessor)
+
     if use_cuda:
         model.cuda()
     model.eval()
@@ -51,13 +58,34 @@ def get_vectors(model: nn.Module, preprocessor: Preprocessor,
                      "textures": all_textures}, o)
 
 
-use_vit = True
-
 if __name__ == "__main__":
-    if use_vit:
-        get_vectors(ViTModel("google/vit-base-patch16-224"),
-                    ViTPreprocessor("google/vit-base-patch16-224"))
-    else:
-        #  model = AlexNetModel("AlexNet_Weights.IMAGENET1K_V1")
-        get_vectors(AlexNetModel("AlexNet_Weights.DEFAULT"),
-                    AlexNetPreprocessor())
+    with open("paths.json", "r") as f:
+        output_folder = Path(json.load(f)["image_vector_output"])
+
+    # Baseline
+    get_vectors(BaselineModel(), BaselinePreprocessor(),
+                output_filename=output_folder / "baseline_vectors.p")
+
+    # AlexNet
+    alexnet_preprocessor = AlexNetPreprocessor()
+    get_vectors(AlexNetModel(), alexnet_preprocessor,
+                output_filename=output_folder / "alexnet_vectors.p")
+    get_vectors(AlexNetModel(None), alexnet_preprocessor,
+                output_filename=output_folder / "alexnet_untrained_vectors.p")
+
+    # ResNet
+    resnet_preprocessor = ResNetPreprocessor()
+    get_vectors(ResNetModel(), resnet_preprocessor,
+                output_filename=output_folder / "resnet_vectors.p")
+    get_vectors(ResNetModel(None), resnet_preprocessor,
+                output_filename=output_folder / "resnet_untrained_vectors.p")
+
+    # ViT
+    vit_preprocessor = ViTPreprocessor("google/vit-base-patch32-224-in21k")
+    get_vectors(ViTModel("google/vit-base-patch32-224-in21k"),
+                vit_preprocessor,
+                output_filename=output_folder / "vit_vectors.p")
+    get_vectors(ViTModel("google/vit-base-patch32-224-in21k",
+                         pretrained=False),
+                vit_preprocessor,
+                output_filename=output_folder / "vit_untrained_vectors.p")

4952daad60d0279e99e7e9961daaad8363ed4cec
Sophie Hao <yiding.hao@yale.edu>
2023-03-01 10:10:34 -0500

    Added preprocessor abstraction

---
+++ preprocessors.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+from typing import List
+from typing import Optional
+
+import torch
+from PIL import Image
+from torchvision import transforms
+from transformers import ViTImageProcessor
+
+
+class Preprocessor(ABC):
+    """
+    Wrapper around various image processors
+    """
+
+    def __call__(self, images: List[Image.Image]) -> torch.Tensor:
+        return self.process_images(images)
+
+    @abstractmethod
+    def process_images(self, images: List[Image.Image]) -> torch.Tensor:
+        raise NotImplementedError("process_images needs to be implemented")
+
+
+class AlexNetPreprocessor(Preprocessor):
+    """
+    Preprocessing pipeline for AlexNet
+    """
+
+    def __init__(self, model_name: Optional[str] = None):
+        _ = model_name
+        self.pipeline = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([.485, .456, .406], [.229, .224, .225])])
+
+    def process_images(self, images: List[Image.Image]) -> torch.Tensor:
+        return torch.cat([self.pipeline(i).unsqueeze(0) for i in images])
+
+
+class ViTPreprocessor(Preprocessor):
+    """
+    Preprocessing pipeline for ViT
+    """
+
+    def __init__(self, model_name: Optional[str] = None):
+        self.processor = ViTImageProcessor.from_pretrained(model_name)
+        self.processor.do_rescale = False
+        self.processor.do_resize = False
+
+    def process_images(self, images: List[Image.Image]) -> torch.Tensor:
+        return self.processor(images, return_tensors="pt")["pixel_values"]

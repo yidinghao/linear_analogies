badc0fec99cb4bb134d65294bfb3ca65bf675b5c
Sophie Hao <yiding.hao@yale.edu>
2023-03-01 10:36:50 -0500

    Added baseline and resnet models

--- models.py
+++ models.py
@@ -2,8 +2,17 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
-import torchvision.models as models
+import torchvision.models as torchvision_models
 from transformers import ViTModel as HuggingFaceViTModel
+
+
+class BaselineModel(nn.Module):
+    """
+    Just returns the pixel values.
+    """
+
+    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
+        return torch.flatten(pixel_values, start_dim=1)
 
 
 class AlexNetModel(nn.Module):
@@ -11,13 +20,36 @@ class AlexNetModel(nn.Module):
     A wrapper around torchvision's AlexNet.
     """
 
-    def __init__(self, model_name: Optional[str] = None):
+    def __init__(self, model_name: Optional[str] = "DEFAULT"):
         super(AlexNetModel, self).__init__()
-        self.alexnet = models.alexnet(weights=model_name).features
+        self.alexnet = torchvision_models.alexnet(weights=model_name).features
 
     def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
-        batch_size = len(pixel_values)
-        return self.alexnet(pixel_values).view(batch_size, -1)
+        return torch.flatten(self.alexnet(pixel_values), start_dim=1)
+
+
+class ResNetModel(nn.Module):
+    """
+    A wrapper around torchvision's ResNet-50.
+    """
+
+    def __init__(self, model_name: Optional[str] = "DEFAULT"):
+        super(ResNetModel, self).__init__()
+        self.resnet = torchvision_models.resnet50(weights=model_name).features
+
+    def forward(self, pixel_values: torch.Tensor) -> torch.Tensor:
+        pixel_values = self.resnet.conv1(pixel_values)
+        pixel_values = self.resnet.bn1(pixel_values)
+        pixel_values = self.resnet.relu(pixel_values)
+        pixel_values = self.resnet.maxpool(pixel_values)
+
+        pixel_values = self.resnet.layer1(pixel_values)
+        pixel_values = self.resnet.layer2(pixel_values)
+        pixel_values = self.resnet.layer3(pixel_values)
+        pixel_values = self.resnet.layer4(pixel_values)
+
+        pixel_values = self.resnet.avgpool(pixel_values)
+        return torch.flatten(pixel_values, 1)
 
 
 class ViTModel(nn.Module):

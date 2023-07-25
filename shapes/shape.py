b16db1c461f4aaf551819a909cb17ea73b857468
Sophie Hao <yiding.hao@yale.edu>
2023-07-25 15:47:29 -0400

    Created data types for color and texture

--- shapes/shape.py
+++ shapes/shape.py
@@ -1,53 +1,87 @@
 """
 Code for generating pictures of shapes on a gray background.
 """
-import math
 import random
-from collections import namedtuple
 from pathlib import Path
-from typing import KeysView, List, NamedTuple, Optional, Tuple, Union
+from typing import List, NamedTuple, Optional, Tuple
 
-import numpy as np
-from PIL import Image, ImageDraw, ImageChops, ImageOps, ImageStat, ImageEnhance
+from PIL import Image, ImageDraw, ImageOps, ImageStat, ImageEnhance
 
-Color = Union[Tuple[int, int, int], Tuple[int, int, int, int]]
+canvas_size = (225, 225)
 
 
-class Textures:
+class Color:
     """
-    A container for textures
+    Data structure for colors
     """
 
-    _brightness = 512 / 3 - 1
+    names = {"red": (255, 0, 0),
+             "green": (0, 255, 0),
+             "blue": (0, 0, 255),
+             "black": (0, 0, 0),
+             "white": (255, 255, 255),
+             "gray": (127, 127, 127)}
 
-    def __init__(self, *texture_names: str):
-        self._textures = {tn: None for tn in texture_names}
-        self.textures_dir = Path("shapes/textures")
+    def __init__(self, *args, alpha: Optional[int] = None):
+        """
+        Creates a color.
 
-    def __contains__(self, item: str):
-        return item in self._textures
+        :param args: The color's name (str) or RGB value (int). For RGB,
+            put the 3 numbers as separate arguments. For the color name,
+            use the keys of Color.names
+        :param alpha: The color's alpha value
+        """
+        self.alpha = alpha
+        if len(args) == 1 and args[0] in self.names:
+            self.rgb = self.names[args[0]]
+        elif all(isinstance(a, int) for a in args) and len(args) == 3:
+            self.rgb = args
+        elif all(isinstance(a, int) for a in args) and len(args) == 4:
+            self.rgb = args[:3]
+            if alpha is None:
+                self.alpha = args[3]
+        else:
+            raise ValueError("{} is not a valid color".format(",".join(args)))
 
-    def __iter__(self):
-        return self._textures.__iter__()
+    def __call__(self) -> Tuple[int, ...]:
+        return self.rgb if self.alpha is None else self.rgb + (self.alpha,)
 
-    def __len__(self):
-        return len(self._textures)
+    def __repr__(self):
+        if self.alpha is None:
+            return "Color({}, {}, {})".format(*self.rgb, self.alpha)
+        return "Color({}, {}, {}, alpha={})".format(*self.rgb, self.alpha)
 
-    def __getitem__(self, item: str):
-        if self._textures[item] is None:
-            self._textures[item] = Textures._load_texture(item)
-        return self._textures[item]
+    def __hash__(self):
+        return hash(self.rgb + (self.alpha,))
 
-    @classmethod
-    def _load_texture(cls, texture_name: str) -> Image:
-        """ Loads a texture from a jpeg file """
-        fn = cls.textures_dir / "{}.jpeg".format(texture_name)
-        img = ImageOps.grayscale(Image.open(fn).resize((224, 224)))
-        factor = cls._brightness / ImageStat.Stat(img).mean[0]
-        return ImageEnhance.Brightness(img).enhance(factor).convert("RGB")
+    def __eq__(self, other: "Color") -> bool:
+        return self.rgb == other.rgb and self.alpha == other.alpha
 
-    def keys(self) -> KeysView:
-        return self._textures.keys()
+
+class Texture:
+    """
+    Data structure for textures
+    """
+    _brightness = 512. / 3. - 1.
+    _cache = dict()
+    dir = Path("shapes/textures")
+
+    def __init__(self, texture_name: str):
+        self.name = texture_name
+
+    def __repr__(self):
+        return "Texture({})".format(self.name)
+
+    def image(self):
+        if self.name in self._cache:
+            return self._cache[self.name]
+        else:
+            fn = self.dir / "{}.jpeg".format(self.name)
+            img = ImageOps.grayscale(Image.open(fn).resize((224, 224)))
+            factor = self._brightness / ImageStat.Stat(img).mean[0]
+            img = ImageEnhance.Brightness(img).enhance(factor).convert("RGB")
+            self._cache[self.name] = img
+            return img
 
 
 class Shape(NamedTuple):
@@ -56,45 +90,39 @@ class Shape(NamedTuple):
     """
     shape: str
     color: Color
-    center_x: int
-    center_y: int
+    x: int
+    y: int
     radius: int
     rotation: int
-    texture: Optional[str] = None
+    texture: Optional[Texture] = None
 
     """ Shape properties """
 
     shapes = ("circle", "square", "triangle")
-    colors = {"red": (225, 0, 0), "green": (0, 225, 0), "blue": (0, 0, 225)}
-    max_x = 225
-    max_y = 225
+    colors = (Color("red"), Color("green"), Color("blue"))
+    max_x = canvas_size[0] - 1
+    max_y = canvas_size[1] - 1
     min_radius = 20
-    textures = Textures("blotchy", "knitted", "lacelike", "marbled", "porous")
+    textures = (Texture("blotchy"), Texture("knitted"), Texture("lacelike"),
+                Texture("marbled"), Texture("porous"))
+
+    @classmethod
+    def max_radius(cls, x: int, y: int) -> int:
+        return min(x, y, cls.max_x - x, cls.max_y - y)
 
     """ Generate a shape """
 
-    @classmethod
-    def _generate_shape(cls, exclude: Optional[str] = None):
+    @staticmethod
+    def _sample(options, exclude=None):
         if exclude is None:
-            return random.choice(cls.shapes)
-        return random.choice([s for s in cls.shapes if s != exclude])
+            return random.choice(options)
+        return random.choice([i for i in options if i != exclude])
 
     @classmethod
-    def _generate_color(cls, exclude: Optional[Color] = None) -> Color:
-        color_names = [cn for cn, c in cls.colors.items() if c != exclude]
-        return cls.colors[random.choice(color_names)]
-
-    @classmethod
-    def _generate_x(cls, radius: Optional[int] = None) -> int:
+    def _generate_xy(cls, max_xy: int, radius: Optional[int] = None) -> int:
         if radius is None:
             radius = cls.min_radius
-        return random.randint(radius, cls.max_x - radius)
-
-    @classmethod
-    def _generate_y(cls, radius: Optional[int] = None) -> int:
-        if radius is None:
-            radius = cls.min_radius
-        return random.randint(radius, cls.max_y - radius)
+        return random.randint(radius, max_xy - radius)
 
     @classmethod
     def _generate_radius(cls, max_radius: Optional[int] = None) -> int:
@@ -107,14 +135,10 @@ class Shape(NamedTuple):
         return random.randint(0, 359)
 
     @classmethod
-    def _generate_texture(cls, exclude: Optional[str] = None):
-        return random.choice([k for k in cls.textures if k != exclude])
-
-    @classmethod
     def generate(cls, shape: Optional[str] = None,
-                 color: Optional[Union[Color, str]] = None,
-                 center_x: Optional[int] = None,
-                 center_y: Optional[int] = None,
+                 color: Optional[Color] = None,
+                 x: Optional[int] = None,
+                 y: Optional[int] = None,
                  radius: Optional[int] = None,
                  rotation: Optional[int] = None,
                  texture: Optional[str] = None,
@@ -122,72 +146,50 @@ class Shape(NamedTuple):
         """
         Generate a random shape with desired properties.
         """
-        if shape is None:
-            shape = cls._generate_shape()
-
-        if color is None:
-            color = cls._generate_color()
-        elif isinstance(color, str):
-            color = cls.colors[color]
-
-        if center_x is None:
-            center_x = cls._generate_x()
-
-        if center_y is None:
-            center_y = cls._generate_y()
-
+        shape = cls._sample(cls.shapes) if shape is None else shape
+        color = cls._sample(cls.colors) if color is None else color
+        x = cls._generate_xy(cls.max_x) if x is None else x
+        y = cls._generate_xy(cls.max_y) if y is None else y
+        rotation = cls._generate_rotation() if rotation is None else rotation
         if radius is None:
-            max_radius = min(center_x, center_y, cls.max_x - center_x,
-                             cls.max_y - center_y)
-            radius = cls._generate_radius(max_radius=max_radius)
-
-        if rotation is None:
-            rotation = cls._generate_rotation()
-
+            radius = cls._generate_radius(max_radius=cls.max_radius(x, y))
         if not no_texture and texture is None:
-            texture = cls._generate_texture()
+            texture = cls._sample(cls.textures)
 
-        return cls(shape, color, center_x, center_y, radius, rotation,
-                   texture=texture)
+        return cls(shape, color, x, y, radius, rotation, texture=texture)
 
     def perturb(self, features: List[str]) -> "Shape":
         kwargs = {}
         if "shape" in features:
-            kwargs["shape"] = self._generate_shape(exclude=self.shape)
+            kwargs["shape"] = self._sample(self.shapes, exclude=self.shape)
         if "color" in features:
-            kwargs["color"] = self._generate_color(exclude=self.color)
-        if "center_x" in features:
-            center_x = self._generate_x(radius=self.radius)
-            kwargs["center_x"] = center_x
-        else:
-            center_x = self.center_x
-        if "center_y" in features:
-            center_y = self._generate_y(radius=self.radius)
-            kwargs["center_y"] = self._generate_y(radius=self.radius)
-        else:
-            center_y = self.center_y
+            kwargs["color"] = self._sample(self.colors, exclude=self.color)
+        if "x" in features:
+            kwargs["x"] = self._generate_xy(self.max_x, radius=self.radius)
+        if "y" in features:
+            kwargs["y"] = self._generate_xy(self.max_y, radius=self.radius)
         if "radius" in features:
-            max_radius = min(center_x, center_y, Shape.max_x - center_x,
-                             Shape.max_y - center_y)
-            kwargs["radius"] = self._generate_radius(max_radius)
+            x = self.x if "x" not in kwargs else kwargs["x"]
+            y = self.y if "y" not in kwargs else kwargs["y"]
+            kwargs["radius"] = self._generate_radius(self.max_radius(x, y))
         if "rotation" in features:
             kwargs["rotation"] = self._generate_rotation()
         if "texture" in features:
-            kwargs["texture"] = self._generate_texture(exclude=self.texture)
+            kwargs["texture"] = self._sample(self.textures,
+                                             exclude=self.texture)
 
         return self._replace(**kwargs)
 
     """ Draw the shape """
 
     def _draw_circle(self, drawer: ImageDraw):
-        drawer.ellipse((self.center_x - self.radius,
-                        self.center_y - self.radius,
-                        self.center_x + self.radius,
-                        self.center_y + self.radius), fill=self.color)
+        drawer.ellipse((self.x - self.radius, self.y - self.radius,
+                        self.x + self.radius, self.y + self.radius),
+                       fill=self.color())
 
     def _draw_polygon(self, drawer: ImageDraw, n: int):
-        drawer.regular_polygon((self.center_x, self.center_y, self.radius),
-                               n, rotation=self.rotation, fill=self.color)
+        drawer.regular_polygon((self.x, self.y, self.radius),
+                               n, rotation=self.rotation, fill=self.color())
 
     def _draw_shape(self, drawer: ImageDraw):
         if self.shape == "circle":
@@ -200,7 +202,7 @@ class Shape(NamedTuple):
             raise NotImplementedError("Drawing shape {} is currently not "
                                       "supported".format(self.shape))
 
-    def draw(self, bg_color: Color = (128, 128, 128)) -> Image:
+    def draw(self, bg_color: Color = Color("gray")) -> Image:
         """
         Draws this shape.
 
@@ -208,56 +210,16 @@ class Shape(NamedTuple):
         :return: The image with the shape in it
         """
         # Draw the basic shape
-        shape_image = Image.new("RGB", (224, 224), bg_color)
+        shape_image = Image.new("RGB", (self.max_x, self.max_y), bg_color())
         self._draw_shape(ImageDraw.Draw(shape_image))
 
         if self.texture is None:
             return shape_image
 
         # Draw texture
-        mask = Image.new("RGBA", (224, 224), (0, 0, 0, 255))
-        self._replace(color=(225, 225, 225, 86))._draw_shape(
+        mask = Image.new("RGBA", (self.max_x, self.max_y), (0, 0, 0, 255))
+        self._replace(color=Color(225, 225, 225, 86))._draw_shape(
             ImageDraw.Draw(mask))
 
-        return Image.composite(shape_image, Shape.textures[self.texture],
+        return Image.composite(shape_image, self.texture.image(), \
                                mask).convert("RGB")
-
-
-""" Calculate the overlap between two shapes """
-
-# pixels: Overlap in pixels
-# percentage: Overlap in percentage of combined area
-# percentage1: Overlap in percentage of img1's area
-# percentage2: Overlap in percentage of img2's area
-Overlap = namedtuple("Overlap", "pixels percentage percentage1 percentage2")
-
-
-def _get_area(img: Image) -> int:
-    return np.asarray(img, dtype="int8").sum()
-
-
-def calculate_overlap(s1: Shape, s2: Shape) -> Overlap:
-    """
-    Calculates how much overlap there is between two shapes.
-
-    :param s1: A shape
-    :param s2: Another shape image
-    :return: The amount of overlap between s1 and s2
-    """
-    # First check for zero overlap
-    d = math.dist((s1.center_x, s1.center_y), (s2.center_x, s2.center_y))
-    if d > s1.radius + s2.radius + .01:
-        return Overlap(0, 0., 0., 0.)
-
-    # Now draw the two pictures and calculate the overlap
-    white = (255, 255, 255)
-    black = (0, 0, 0)
-    img1 = s1._replace(color=white).draw(bg_color=black).convert("1")
-    img2 = s2._replace(color=white).draw(bg_color=black).convert("1")
-
-    area1 = _get_area(img1)
-    area2 = _get_area(img2)
-    and_ = _get_area(ImageChops.logical_and(img1, img2))
-    or_ = _get_area(ImageChops.logical_or(img1, img2))
-
-    return Overlap(and_, and_ / or_, and_ / area1, and_ / area2)
